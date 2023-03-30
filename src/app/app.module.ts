1a7f8a49f4a568f2a8515c8b10c4646223900015
Alex Erdei <mralexerdei@yahoo.co.uk>
2023-03-30 01:41:43 +0100

    Add basic data and url method to weather component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,14 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { WeatherComponentComponent } from './weather/weather-component/weather-component.component';
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    WeatherComponentComponent
-  ],
-  imports: [
-    BrowserModule
-  ],
+  declarations: [AppComponent, WeatherComponentComponent],
+  imports: [BrowserModule, FormsModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
--- src/app/weather/weather-component/weather-component.component.ts
+++ src/app/weather/weather-component/weather-component.component.ts
@@ -1,10 +1,21 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-weather-component',
   templateUrl: './weather-component.component.html',
-  styleUrls: ['./weather-component.component.css']
+  styleUrls: ['./weather-component.component.css'],
 })
-export class WeatherComponentComponent {
+export class WeatherComponentComponent implements OnInit {
+  public cityName!: string;
+  public temperature!: number;
+  constructor() {}
 
+  ngOnInit(): void {
+    this.cityName = 'Budapest'; //Initialise with a city
+    this.temperature = 0;
+  }
+
+  private url(city: string): string {
+    return `https://api.openweathermap.org/data/2.5/weather?q=${city}&APPID=db5f1eb9b2cd1a0d24607c0b04e5887b`;
+  }
 }

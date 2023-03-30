ab5a2bd674585158eb50d542030c97e4916a4bd5
Alex Erdei <mralexerdei@yahoo.co.uk>
2023-03-30 01:09:11 +0100

    Add weather component to app

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { WeatherComponentComponent } from './weather/weather-component/weather-component.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    WeatherComponentComponent
   ],
   imports: [
     BrowserModule

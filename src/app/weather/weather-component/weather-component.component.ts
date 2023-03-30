a8b62c041ef96578f387074bea1368839ae1d63f
Alex Erdei <mralexerdei@yahoo.co.uk>
2023-03-30 02:47:06 +0100

    Make form work

--- src/app/weather/weather-component/weather-component.component.ts
+++ src/app/weather/weather-component/weather-component.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Observable } from 'rxjs';
+import { City } from 'src/app/model/city';
 
 @Component({
   selector: 'app-weather-component',
@@ -6,16 +8,27 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./weather-component.component.css'],
 })
 export class WeatherComponentComponent implements OnInit {
-  public cityName!: string;
-  public temperature!: number;
-  constructor() {}
+  public city!: City;
+  public message!: string;
+  public data!: Observable<Object>;
+  constructor() {
+    this.city = { name: 'Budapest', temperature: 0 }; //Initialise with a city
+  }
 
   ngOnInit(): void {
-    this.cityName = 'Budapest'; //Initialise with a city
-    this.temperature = 0;
+    this.message = 'Initial values';
   }
 
   private url(city: string): string {
     return `https://api.openweathermap.org/data/2.5/weather?q=${city}&APPID=db5f1eb9b2cd1a0d24607c0b04e5887b`;
+  }
+
+  changeCity(cityForm: { valid: any }) {
+    if (cityForm.valid) {
+      console.log(this.city);
+    } else {
+      this.message = 'Form is invalid!';
+      console.error(new Error(this.message));
+    }
   }
 }
